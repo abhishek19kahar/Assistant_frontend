798e2cbcebe085701767aa3dbdf2b3f7921328be
abhishek19kahar <125858848+abhishek19kahar@users.noreply.github.com>
2025-10-18 23:22:00 +0530

    Update API URLs in UserContext

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -11,7 +11,7 @@ function UserContext({ children }) {
 
   const handleCurrentUser = async () => {
     try {  //https://assistant-backend-5qny.onrender.com
-      const result = await axios.get('https://assistantbackend-production-7039.up.railway.app/api/user/current', {
+      const result = await axios.get('https://assistant-backend-5qny.onrender.com/api/user/current', {
         withCredentials: true,
       });
       setUserData(result.data);
@@ -31,7 +31,7 @@ function UserContext({ children }) {
     try {
       const result = await axios.post(
         //https://assistantbackend-production-7039.up.railway.app/
-        'https://assistantbackend-production-7039.up.railway.app/api/user/asktoassistant',
+        'https://assistant-backend-5qny.onrender.com/api/user/asktoassistant',
         { command },
         { withCredentials: true }
       );
